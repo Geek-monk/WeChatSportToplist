c687ac047c1d9aed6e9b0dd2b34a4ee8bf79555e
Geekmonk <bg2cop@gmail.com>
2022-03-07 22:09:18 +0800

    修改每天步数的最大值

--- main.py
+++ main.py
@@ -44,9 +44,9 @@ time_list = [8, 10, 13, 15, 17, 19, 21]
 set_push = [True, True, True, True, True, False, False]
 
 # 最小步数（如果只需要刷步的次数少于7次就将该次数以后的步数全都改成0，如：time_list[3]: 0，表示第五次开始不运行或者直接云函数触发里面不在该时间调用均可（建议用后者））
-min_dict = {time_list[0]: 3000, time_list[1]: 5500, time_list[2]: 10000, time_list[3]: 15000, time_list[4]: 20000, time_list[5]: 23000, time_list[6]: 28000}
+min_dict = {time_list[0]: 3000, time_list[1]: 5500, time_list[2]: 10000, time_list[3]: 15000, time_list[4]: 20000, time_list[5]: 0, time_list[6]: 0}
 # 最大步数（例如现在设置意思是在8点（你设置的第一个时间点默认8）运行会在1500到2999中随机生成一个数提交（开启气候降低步数会乘系数K）10点3000~4999。。。以此类推，步数范围建议看懂了再改，没看懂直接默认就好）
-max_dict = {time_list[0]: 3999, time_list[1]: 6999, time_list[2]: 13000, time_list[3]: 18000, time_list[4]: 22000, time_list[5]: 27000, time_list[6]: 30000}
+max_dict = {time_list[0]: 3999, time_list[1]: 6999, time_list[2]: 13000, time_list[3]: 18000, time_list[4]: 23000, time_list[5]: 0, time_list[6]: 0}
 # 设置结束
 #now = time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())
 # 北京时间
